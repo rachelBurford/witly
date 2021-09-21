7fd94f4412f351b3b2d63b8f0822d39138e38dc3
rachelBurford <rachel@pilbaratax.com.au>
2021-09-21 19:39:54 +0800

    prep app for deploy on
    HEROKU

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.set('view engine', 'ejs');
 app.set('views', './views/pages/');
 app.use(express.static('public'));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server started!')
 });
 
