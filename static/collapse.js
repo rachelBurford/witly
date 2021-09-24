3da5bbcb358eaee6f47eeed9e31855d912326808
rachelBurford <rachel@pilbaratax.com.au>
2021-09-24 16:21:44 +0800

    update
    formatting

--- static/collapse.js
+++ static/collapse.js
@@ -1,10 +1,10 @@
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementById("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    var content = document.getElementById("content");
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
